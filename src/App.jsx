1d09d5238539c2494d3dbc8fcde8398bb1fec21c
kioiti <kioiti@naver.com>
2025-11-27 11:46:21 +0900

    feat:add to Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
-import Mainpage from "./Pages/Mainpage";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/home";
+import Detail from "./Pages/detail";
 
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Mainpage />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                {/* 메인페이지 */}
+                <Route path="/" element={<Home />} />
+                <Route path="/Detail" element={<Detail />} />
+                {/* <Route path="/Study" element={<Study />} />
+                <Route path="/Habit" element={<Habit />} /> */}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
