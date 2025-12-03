14b29bdbf64c973d30ab1aacf5a64c91414094a2
kioiti <kioiti@naver.com>
2025-12-03 16:35:54 +0900

    feat:add to emoji dr

---
+++ src/components/molecule/emoji/EmojiGroup.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import EmojiButton from "./EmojiButton";
+import EmojiPickerWrapper from "./EmojiPickerWrapper";
+import "@styles/EmojiGroup.css";
+
+export default function EmojiGroup({ reactions, onEmojiClick, onAddEmoji }) {
+    return (
+        <div className="emoji-group">
+            <div className="emoji-group__inner">
+                {reactions.map((item) => (
+                    <EmojiButton
+                        key={item.id}
+                        emoji={item.emoji}
+                        count={item.count}
+                        isActive={item.me}
+                        onClick={() => onEmojiClick?.(item.emoji)}
+                    />
+                ))}
+                <EmojiPickerWrapper onSelect={onAddEmoji}>
+                    <EmojiButton variant="add" />
+                </EmojiPickerWrapper>
+            </div>
+        </div>
+    );
+}
