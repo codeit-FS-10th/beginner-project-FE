598c13ef1adf7cc040057456285aa1c6ff8f7c37
wanderingsunbi <neoguysm@gmail.com>
2025-12-03 13:24:15 +0900

    Dropdown atom updated

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -5,3 +5,4 @@ export { default as StopIcon } from "./StopIcon";
 export { default as LeafIcon } from "./LeafIcon";
 export { default as TrashIcon } from "./TrashIcon";
 export { default as ArrowIcon } from "./ArrowIcon";
+export { default as ToggleIcon } from "./ToggleIcon";
