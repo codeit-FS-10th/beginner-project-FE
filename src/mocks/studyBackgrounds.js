2985a0796dbd72d8389ba4409055dab7477f0d5a
김명환 <lostalgia.kr@gmail.com>
2025-12-06 16:43:17 +0900

    background 수정

--- src/mocks/studyBackgrounds.js
+++ src/mocks/studyBackgrounds.js
@@ -1,19 +1,19 @@
-import bg1 from "@img/mockImg/card_background1.jpg";
-import bg2 from "@img/mockImg/card_background2.jpg";
-import bg3 from "@img/mockImg/card_background3.jpg";
-import bg4 from "@img/mockImg/card_background4.jpg";
-import bg5 from "@img/mockImg/card_background5.jpg";
-import bg6 from "@img/mockImg/card_background6.jpg";
-import bg7 from "@img/mockImg/card_background7.jpg";
-import bg8 from "@img/mockImg/card_background8.jpg";
+import green from "@img/mockImg/green.jpg";
+import yellow from "@img/mockImg/yellow.jpg";
+import blue from "@img/mockImg/blue.jpg";
+import pink from "@img/mockImg/pink.jpg";
+import mouseTablet from "@img/mockImg/mouse_tablet.jpg";
+import tabletBottle from "@img/mockImg/tablet_bottle.jpg";
+import diamond from "@img/mockImg/diamond.jpg";
+import leaf from "@img/mockImg/leaf.jpg";
 
 export const mockBackgrounds = [
-    { id: 1, image: bg1 },
-    { id: 2, image: bg2 },
-    { id: 3, image: bg3 },
-    { id: 4, image: bg4 },
-    { id: 5, image: bg5 },
-    { id: 6, image: bg6 },
-    { id: 7, image: bg7 },
-    { id: 8, image: bg8 },
+    { id: 1, code: "green", image: green },
+    { id: 2, code: "yellow", image: yellow },
+    { id: 3, code: "blue", image: blue },
+    { id: 4, code: "pink", image: pink },
+    { id: 5, code: "mouse_tablet", image: mouseTablet },
+    { id: 6, code: "tablet_bottle", image: tabletBottle },
+    { id: 7, code: "diamond", image: diamond },
+    { id: 8, code: "leaf", image: leaf },
 ];
